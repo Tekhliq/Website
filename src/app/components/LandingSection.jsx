4082e602dbf08015de192ba0848e3aee4c9fd916
Tekhliq Labs <158844397+Tekhliq@users.noreply.github.com>
2024-03-09 04:50:23 +0500

    Update the Landing section message

--- src/app/components/LandingSection.jsx
+++ src/app/components/LandingSection.jsx
@@ -7,8 +7,7 @@ const LandingSection = () => {
     <section style={{height: '100vh'}} className="flex flex-col items-center py-14 px-36 border-b-[1px] border-black border-opacity-20">
       <h1 className="text-black text-6xl font-semibold  mt-32">Welcome to Tekhliq Labs</h1>
       <p style={{width: '40vw'}} className="text-black text-2xl mt-6 text-center">
-        We are a Generative AI company that is focused on creating the most
-        advanced AI models to help you create amazing content.
+        Engineering the Art of Creation 🔆
       </p>
     </section>
   );
