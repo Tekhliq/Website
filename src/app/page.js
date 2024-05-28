e93a52d80dacb47613434e68dd7b63cd67163d10
umman <ummanw@yahoo.com>
2024-05-28 16:43:32 +0500

    adjust getstarted

--- src/app/page.js
+++ src/app/page.js
@@ -32,7 +32,7 @@ export default function Component() {
         <section className="absolute top-0 w-full min-h-screen py-12 md:py-24 lg:py-32 xl:py-48">
           <div className="container px-4 md:px-6">
             <div className="flex flex-col items-center space-y-4 text-center">
-              <div className="space-y-2 mb-10">
+              <div className="space-y-2 mb-4">
                 <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
                   Unlock the Power of
                 </h1>
